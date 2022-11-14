6a36f55d1664ebcb11a67b0d4a48bc398a74854b
numeru <numeru@naver.com>
2022-11-14 21:02:13 +0900

    💄 design : 스타일 기본 세팅 추가

---
+++ src/styles/commons.ts
@@ -0,0 +1,61 @@
+import { css } from '@emotion/react';
+
+export const Header1 = css`
+  font-size: 22pt;
+  font-family: 'pretendard_medium';
+`;
+
+export const Header2 = css`
+  font-size: 20pt;
+  font-family: 'pretendard_bold';
+`;
+
+export const Header3 = css`
+  font-size: 20pt;
+  font-family: 'pretendard_medium';
+`;
+
+export const Header4 = css`
+  font-size: 18pt;
+  font-family: 'pretendard_semiBold';
+`;
+
+export const Header5 = css`
+  font-size: 16pt;
+  font-family: 'pretendard_semiBold';
+`;
+
+export const Body1 = css`
+  font-size: 18pt;
+  font-family: 'pretendard_medium';
+`;
+
+export const Body2 = css`
+  font-size: 16pt;
+  font-family: 'pretendard_medium';
+`;
+
+export const Body3 = css`
+  font-size: 14pt;
+  font-family: 'pretendard_semiBold';
+`;
+
+export const Body4 = css`
+  font-size: 14pt;
+  font-family: 'pretendard_medium';
+`;
+
+export const Body5 = css`
+  font-size: 14pt;
+  font-family: 'pretendard_regular';
+`;
+
+export const Caption1 = css`
+  font-size: 12pt;
+  font-family: 'pretendard_semiBold';
+`;
+
+export const Caption2 = css`
+  font-size: 12pt;
+  font-family: 'pretendard_medium';
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { Theme } from '@emotion/react';
+
+const theme: Theme = {
+  colors: {
+    white: '#ffffff',
+  },
+};
+
+export default theme;
