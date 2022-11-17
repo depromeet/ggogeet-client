ffe98a30e93547253c40962fd7f0d1d4789633ff
ding-co <pangoons@naver.com>
2022-11-18 00:12:09 +0900

    ♻️ 컴포넌트 폴더 구조 리팩토링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
-import { NavBell, NavLogo } from '../components/common/atoms';
-import GNB from '../components/common/GNB';
+import { NavBell, NavLogo } from '../components/common/gnb';
+import GNB from '../components/common/gnb/GNB';
 
 const MainPage: NextPage = () => {
   return (
