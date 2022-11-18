569fd5547659d05a889252d79fb4cf289c3c8f15
numeru <numeru@naver.com>
2022-11-18 17:45:14 +0900

    ✨ tag 공통 컴포넌트 추가

---
+++ src/components/common/Tag/styled.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import { Caption1 } from '@/src/styles/commons';
+
+export const TagWrapper = styled.span`
+  padding: 4px 12px;
+  background: #f0f2f7;
+  color: #989fb3;
+  border-radius: 14px;
+  line-height: 14px;
+
+  ${Caption1}
+`;
