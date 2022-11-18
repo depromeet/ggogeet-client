74c45edd141848ff45b2f2d24a3073cf163befe8
최영광 <numeru@naver.com>
2022-11-18 13:10:20 +0900

    💄 스타일 기본 세팅 추가 (#9)
    * 💄 design : 스타일 기본 세팅 추가
    
    * 💄 폰트 단위 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { Global, ThemeProvider } from '@emotion/react';
+import theme from '@/src/styles/theme';
+import { global } from '@/src/styles/global';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={global} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,103 @@
+import { css, Theme } from '@emotion/react';
+
+export const global = (theme: Theme) => css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    margin: auto;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+    padding: 0;
+  }
+
+  h1,
+  h2,
+  h3 {
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  input,
+  textarea {
+    appearance: none;
+    outline: none;
+  }
+
+  textarea {
+    resize: none;
+  }
+
+  .a11y-hidden {
+    position: absolute;
+    clip: rect(0, 0, 0, 0);
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
+    margin: -1px;
+    border: 0;
+    padding: 0;
+    white-space: nowrap;
+  }
+
+  @font-face {
+    font-family: 'pretendard_regular';
+    src: url('/assets/fonts/Pretendard-Regular.woff2') format('woff2'),
+      url('/assets/fonts/Pretendard-Regular.woff') format('woff'),
+      url('/assets/fonts/Pretendard-Regular.ttf') format('ttf');
+    font-display: swap;
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'pretendard_medium';
+    src: url('/assets/fonts/Pretendard-Medium.woff2') format('woff2'),
+      url('/assets/fonts/Pretendard-Medium.woff') format('woff'),
+      url('/assets/fonts/Pretendard-Medium.ttf') format('ttf');
+    font-display: swap;
+    font-weight: 500;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'pretendard_semiBold';
+    src: url('/assets/fonts/Pretendard-SemiBold.woff2') format('woff2'),
+      url('/assets/fonts/Pretendard-SemiBold.woff') format('woff'),
+      url('/assets/fonts/Pretendard-SemiBold.ttf') format('ttf');
+    font-display: swap;
+    font-weight: 600;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'pretendard_bold';
+    src: url('/assets/fonts/Pretendard-Bold.woff2') format('woff2'),
+      url('/assets/fonts/Pretendard-Bold.woff') format('woff'),
+      url('/assets/fonts/Pretendard-Bold.ttf') format('ttf');
+    font-display: swap;
+    font-weight: 700;
+    font-style: normal;
+  }
+`;
