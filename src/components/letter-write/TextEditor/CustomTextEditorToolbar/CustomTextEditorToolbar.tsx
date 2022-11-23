4855d255ecd84a8aa9cd5ec5c67d596a6a1cbe17
ding-co <pangoons@naver.com>
2022-11-23 22:56:14 +0900

    💄 CustomTextEditorToolbar 컴포넌트 UI

--- src/components/letter-write/TextEditor/CustomTextEditorToolbar/CustomTextEditorToolbar.tsx
+++ src/components/letter-write/TextEditor/CustomTextEditorToolbar/CustomTextEditorToolbar.tsx
@@ -1,8 +1,26 @@
+import Image from 'next/image';
 import { ReactElement } from 'react';
 import * as S from './styled';
 
+const customTextEditorToolbarMap = {
+  'align-default': '/icons/icon__align-default.svg',
+  'align-left': '/icons/icon__align-left.svg',
+  'align-center': '/icons/icon__align-center.svg',
+  'align-right': '/icons/icon__align-right.svg',
+  'text-bold': '/icons/icon__text-bold.svg',
+  'text-italic': '/icons/icon__text-italic.svg',
+  'text-underline': '/icons/icon__text-underline.svg',
+  'text-strike': '/icons/icon__text-strike.svg',
+};
+
 const CustomTextEditorToolbar = (): ReactElement => {
-  return <S.CustomTextEditorToolbarWrapper></S.CustomTextEditorToolbarWrapper>;
+  return (
+    <S.CustomTextEditorToolbarWrapper>
+      {Object.entries(customTextEditorToolbarMap).map(([key, value]) => (
+        <Image key={key} alt={key} src={value} width={18} height={18} />
+      ))}
+    </S.CustomTextEditorToolbarWrapper>
+  );
 };
 
 export default CustomTextEditorToolbar;
