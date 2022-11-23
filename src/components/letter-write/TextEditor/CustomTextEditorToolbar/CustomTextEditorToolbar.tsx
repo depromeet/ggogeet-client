3f70d0b5430bbb04b340765f96ca7142a6444845
ding-co <pangoons@naver.com>
2022-11-23 22:38:59 +0900

    💄 React Quill design 수정, CustomToolbar setup

---
+++ src/components/letter-write/TextEditor/CustomTextEditorToolbar/CustomTextEditorToolbar.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from 'react';
+import * as S from './styled';
+
+const CustomTextEditorToolbar = (): ReactElement => {
+  return <S.CustomTextEditorToolbarWrapper></S.CustomTextEditorToolbarWrapper>;
+};
+
+export default CustomTextEditorToolbar;
