a21f794462865270eee1da5606cade319738a971
numeru <numeru@naver.com>
2022-11-25 16:56:46 +0900

    ✨ Button 공통 컴포넌트 추가

---
+++ src/components/common/Buttons/Button/index.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from 'react';
+import { FontStyle } from './types';
+import * as S from './styled';
+
+interface Props {
+  name: string;
+  fontStyle: FontStyle;
+  rightImg?: ReactNode;
+  isRound?: boolean;
+  isDark?: boolean;
+  onClick?: () => void;
+}
+
+export default function Button({
+  name,
+  rightImg,
+  fontStyle,
+  isRound = false,
+  isDark = false,
+  onClick,
+}: Props) {
+  return (
+    <S.ButtonWrapper
+      isDark={isDark}
+      fontStyle={fontStyle}
+      isRound={isRound}
+      onClick={onClick}
+    >
+      {name}
+      {rightImg}
+    </S.ButtonWrapper>
+  );
+}
---
+++ src/components/common/Buttons/Button/types.d.ts
@@ -0,0 +1,1 @@
+export type FontStyle = 'light' | 'medium' | 'bold';
