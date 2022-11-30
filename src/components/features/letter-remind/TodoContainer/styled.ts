ea20df96cec6063c92af244661cd786abab0af9c
KimGaeun <greencloud11@hanyang.ac.kr>
2022-11-30 23:14:20 +0900

    💄 ToggleArrowButton 컴포넌트 props 수정

--- src/components/features/letter-remind/TodoContainer/styled.ts
+++ src/components/features/letter-remind/TodoContainer/styled.ts
@@ -1,7 +1,6 @@
 import styled from "@emotion/styled";
 
 export const TodoContainerWrapper = styled.div`
-<<<<<<< HEAD
   padding: 0 16px;
   display: flex;
   flex-direction: column;
@@ -25,13 +24,6 @@ export const TodoContainerContentWrapper = styled.div<TodoContainerContentWrappe
   padding: 12px 10px 12px 30px;
   border-top: ${(props) => (props.isClicked ? "1px solid #a4a9b8" : "none")};
   background-color: antiquewhite; // 임시
-=======
-  padding: 12px 16px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border-radius: 8px;
->>>>>>> c287e9b (💄 리마인드 페이지 투두 컴포넌트 생성)
 `;
 
 export const TodoContainerInnerWrapper = styled.div`
@@ -39,15 +31,12 @@ export const TodoContainerInnerWrapper = styled.div`
   align-items: center;
 `;
 
-<<<<<<< HEAD
 export const ContentUpperWrapper = styled.div`
   margin: 0 0 10px 0;
 `;
 
 export const ContentLowerWrapper = styled.div``;
 
-=======
->>>>>>> c287e9b (💄 리마인드 페이지 투두 컴포넌트 생성)
 export const DateWrapper = styled.div`
   padding: 0 8px 0 0;
 `;
