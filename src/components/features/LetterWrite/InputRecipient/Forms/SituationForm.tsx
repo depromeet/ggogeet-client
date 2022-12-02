a5aa389bad9e2488ed2505900364a7a5eb8a4001
ding-co <pangoons@naver.com>
2022-12-02 21:30:16 +0900

    💄 편지 작성 - 상황 설정 기본 UI 추가

--- src/components/features/LetterWrite/InputRecipient/Forms/SituationForm.tsx
+++ src/components/features/LetterWrite/InputRecipient/Forms/SituationForm.tsx
@@ -1,15 +1,62 @@
 import { letterWriteInputState } from "@/src/store/LetterWrite";
 import { ReactElement } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { useBottomButton } from "../Hooks";
 import * as S from "../styled";
 
+type TemplateType = "축하" | "위로" | "사랑해요" | "응원";
+
+const situationTemplates = [
+  {
+    type: "축하",
+    name: "축하",
+    imageUrl: "",
+  },
+  {
+    type: "위로",
+    name: "위로",
+    imageUrl: "",
+  },
+  {
+    type: "사랑해요",
+    name: "사랑해요",
+    imageUrl: "",
+  },
+  {
+    type: "응원",
+    name: "응원",
+    imageUrl: "",
+  },
+];
+
 const SituationForm = (): ReactElement => {
-  const letterWriteInputObjectState = useRecoilValue(letterWriteInputState);
-  const bottomButton = useBottomButton();
+  const [letterWriteInputObjectState, setLetterWriteInputObjectState] =
+    useRecoilState(letterWriteInputState);
+  const bottomButton = useBottomButton({
+    isDisabled: !letterWriteInputObjectState.situation,
+  });
+  const onClickTemplate = (templateType: TemplateType) => {
+    setLetterWriteInputObjectState((prev) => ({
+      ...prev,
+      situation: templateType,
+    }));
+  };
   return (
     <>
       <S.LetterWriteH1>어떤 마음을 전하고 싶나요?</S.LetterWriteH1>
+      <S.SituationTemplateContainer></S.SituationTemplateContainer>
+      <S.SituationSlick>
+        {situationTemplates.map((st) => (
+          <S.SituationSlickItem
+            key={st.type}
+            onClick={() => onClickTemplate(st.type as TemplateType)}
+            isSelected={st.type === letterWriteInputObjectState.situation}
+          >
+            <div></div>
+            <span>{st.name}</span>
+          </S.SituationSlickItem>
+        ))}
+      </S.SituationSlick>
       {bottomButton}
     </>
   );
--- src/components/features/LetterWrite/InputRecipient/styled.ts
+++ src/components/features/LetterWrite/InputRecipient/styled.ts
@@ -1,5 +1,4 @@
 import { Body1, Caption1, Header1 } from "@/src/styles/commons";
-import { QueryString } from "@/src/types";
 import styled from "@emotion/styled";
 
 const LetterWriteInputRecipientLayoutWrapper = styled.div`
@@ -10,7 +9,7 @@ const LetterWriteInputRecipientLayoutWrapper = styled.div`
 const BottomButtonContainer = styled.div<{ type?: string }>`
   width: calc(100% - 32px);
   position: fixed;
-  bottom: 8px;
+  bottom: 16px;
   margin: 0 auto;
   left: 0;
   right: 0;
@@ -84,6 +83,50 @@ const LetterWriteBody1 = styled.span`
   ${Body1}
 `;
 
+const SituationTemplateContainer = styled.div`
+  margin-top: 20px;
+  width: 326px;
+  height: 341px;
+  background: #e4e7ef;
+  border-radius: 12px;
+`;
+
+const SituationSlick = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  width: 100%;
+  height: 120px;
+  overflow: hidden;
+`;
+
+const SituationSlickItem = styled.button<{ isSelected: boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+  div {
+    width: 92px;
+    height: 92px;
+    background: #e4e7ef;
+    border-radius: 12px;
+    ${({ isSelected }) =>
+      isSelected &&
+      `
+      border: 1px solid #989fb3;
+    `}
+  }
+  span {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+    display: flex;
+    align-items: center;
+    color: #000000;
+  }
+`;
+
 export {
   LetterWriteInputRecipientLayoutWrapper,
   BottomButtonContainer,
@@ -93,4 +136,7 @@ export {
   LetterWriteH1,
   LetterWriteCaption1,
   LetterWriteBody1,
+  SituationTemplateContainer,
+  SituationSlick,
+  SituationSlickItem,
 };
