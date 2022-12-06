93b4abab6b8737e171d20d550a4dc4bd851a3628
dongkyuuuu <desktop@kakao.com>
2022-12-06 23:21:35 +0900

    💄 글로벌 색상 변경

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -12,9 +12,7 @@ declare module "@emotion/react" {
       gray3: string;
       gray4: string;
       gray5: string;
-      gray6: string;
-      gray7: string;
-      gray8: string;
+      background: string;
     };
   }
 }
