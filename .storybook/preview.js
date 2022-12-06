5525b982d5cdad82cd680789a0666e069d79ad81
dongkyuuuu <desktop@kakao.com>
2022-12-06 23:36:47 +0900

    🎨 storybook 폰트 및 emotion theme 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { Global, ThemeProvider } from "@emotion/react";
+import theme from "@/src/styles/theme";
+import { global } from "@/src/styles/global";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +10,13 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Global styles={global} />
+      <Story />
+    </ThemeProvider>
+  ),
+];
