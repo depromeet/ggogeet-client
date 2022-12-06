c44a8bb77172e84366b0f9b10adce3484415dc94
dongkyuuuu <desktop@kakao.com>
2022-12-06 22:45:59 +0900

    💄 grayScale 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,17 @@ import { Theme } from "@emotion/react";
 const theme: Theme = {
   colors: {
     white: "#ffffff",
+    primary: "#FF5252",
+    dark: "#26272E",
+    light: "#F6F6F6",
+    gray1: "#BEBEBE",
+    gray2: "#A6A6A6",
+    gray3: "#999999",
+    gray4: "#808080",
+    gray5: "#666666",
+    gray6: "#4D4D4D",
+    gray7: "#333333",
+    gray8: "#1A1A1A",
   },
 };
 
--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -4,6 +4,17 @@ declare module "@emotion/react" {
   export interface Theme {
     colors: {
       white: string;
+      primary: string;
+      dark: string;
+      light: string;
+      gray1: string;
+      gray2: string;
+      gray3: string;
+      gray4: string;
+      gray5: string;
+      gray6: string;
+      gray7: string;
+      gray8: string;
     };
   }
 }
