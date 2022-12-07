48a612c8944baa7ad3d5da3b7614d3039179acc4
KimGaeun <greencloud11@hanyang.ac.kr>
2022-12-07 18:42:03 +0900

    💄 BackButton 컴포넌트 삭제

--- src/components/common/Buttons/BackButton/styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-export const BackButtonWrapper = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-`;
