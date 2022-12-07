6dbdbfc07103e947400138b378bb6393eb98cf21
KimGaeun <greencloud11@hanyang.ac.kr>
2022-12-07 17:58:23 +0900

    💄 storage-add/who 페이지 bottombutton 추가, 스타일 변경

--- src/pages/letter-storage-add/who.tsx
+++ src/pages/letter-storage-add/who.tsx
@@ -1,13 +1,21 @@
 import BackButton from "@/src/components/common/Buttons/BackButton";
 import BottomButton from "@/src/components/common/Buttons/BottomButton";
-import InputDefault, { InputClear } from "@/src/components/common/Input";
+import { InputClear } from "@/src/components/common/Input";
 import TopNavigation from "@/src/components/common/TopNavigation";
-import { Header1, Header5 } from "@/src/styles/commons";
+import { Body1, Header1, Header5 } from "@/src/styles/commons";
 import styled from "@emotion/styled";
 
 const Layout = styled.div`
-  padding: 12px 16px;
+  display: flex;
+  flex-direction: column;
   height: 100vh;
+`;
+
+const MainLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 12px 16px;
+  flex: 1;
 `;
 
 const CancelButton = styled.button`
@@ -23,27 +31,45 @@ const Title = styled.p`
 `;
 
 const BottomButtonsContainer = styled.div`
-  min-height: 100%;
+  padding: 0 16px 40px 16px;
+`;
+
+const NameInput = styled(InputClear)`
+  ${Body1}
+`;
+
+const FirstBottomButton = styled(BottomButton)`
+  border: 1px solid ${({ theme }) => theme.colors.gray5};
+  margin: 0 0 8px 0;
+  ${Header5}
+`;
+
+const SecondBottomButton = styled(BottomButton)`
+  background-color: ${({ theme }) => theme.colors.gray2};
+  color: ${({ theme }) => theme.colors.white};
+  ${Header5}
 `;
 
 const LetterStorageWhoPage = () => {
   const onClear = () => {}; // 임시 함수
 
   return (
-    <>
+    <Layout>
       <TopNavigation
         leftElem={<BackButton />}
         rightElem={<CancelButton>취소</CancelButton>}
       />
 
-      <Layout>
+      <MainLayout>
         <Title>누구에게 받으셨나요?</Title>
-        <InputClear onClear={onClear} placeholder="이름을 입력해주세요" />
+        <NameInput onClear={onClear} placeholder="이름을 입력해주세요" />
+      </MainLayout>
 
-        {/* <BottomButton name="꼬깃 친구 목록에서 찾기" isRound />
-        <BottomButton name="다음" isRound /> */}
-      </Layout>
-    </>
+      <BottomButtonsContainer>
+        <FirstBottomButton name="꼬깃 친구 목록에서 찾기" isRound />
+        <SecondBottomButton name="다음" isRound />
+      </BottomButtonsContainer>
+    </Layout>
   );
 };
 
