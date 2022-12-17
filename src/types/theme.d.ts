90f90126a2e68daa93dfd7c1e39ea69eff34364a
dongkyuuuu <desktop@kakao.com>
2022-12-17 11:22:45 +0900

    💄 색상, 곡률, 여백, 그리드 추가

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,18 +1,6 @@
 import "@emotion/react";
+import type { ThemeExtends } from "@/src/styles/theme";
 
 declare module "@emotion/react" {
-  export interface Theme {
-    colors: {
-      white: string;
-      primary: string;
-      dark: string;
-      light: string;
-      gray1: string;
-      gray2: string;
-      gray3: string;
-      gray4: string;
-      gray5: string;
-      background: string;
-    };
-  }
+  export interface Theme extends ThemeExtends {}
 }
