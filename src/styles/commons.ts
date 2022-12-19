6f1a43ff6538d13d1cb3a316769bca72cbf210d9
numeru <numeru@naver.com>
2022-12-19 17:35:58 +0900

    💄 타이포그래피 이름 변경

--- src/styles/commons.ts
+++ src/styles/commons.ts
@@ -1,42 +1,48 @@
 import { css } from "@emotion/react";
 
-export const Header1 = css`
+export const Display6 = css`
   font-size: 24px;
-  font-weight: 500;
+  font-weight: 600;
   font-display: swap;
 `;
 
-export const Header2 = css`
+export const Display5 = css`
   font-size: 22px;
-  font-weight: 700;
+  font-weight: 600;
   font-display: swap;
 `;
 
-export const Header3 = css`
+export const Display4 = css`
+  font-size: 20px;
+  font-weight: 600;
+  font-display: swap;
+`;
+
+export const Display3 = css`
   font-size: 20px;
   font-weight: 500;
   font-display: swap;
 `;
 
-export const Header4 = css`
+export const Display2 = css`
   font-size: 18px;
   font-weight: 600;
   font-display: swap;
 `;
 
-export const Header5 = css`
+export const Display1 = css`
   font-size: 16px;
   font-weight: 600;
   font-display: swap;
 `;
 
-export const Body1 = css`
+export const Body5 = css`
   font-size: 18px;
   font-weight: 500;
   font-display: swap;
 `;
 
-export const Body2 = css`
+export const Body4 = css`
   font-size: 16px;
   font-weight: 500;
   font-display: swap;
@@ -48,25 +54,25 @@ export const Body3 = css`
   font-display: swap;
 `;
 
-export const Body4 = css`
+export const Body2 = css`
   font-size: 14px;
   font-weight: 500;
   font-display: swap;
 `;
 
-export const Body5 = css`
+export const Body1 = css`
   font-size: 14px;
   font-weight: 400;
   font-display: swap;
 `;
 
-export const Caption1 = css`
+export const Label2 = css`
   font-size: 12px;
   font-weight: 600;
   font-display: swap;
 `;
 
-export const Caption2 = css`
+export const Label1 = css`
   font-size: 12px;
   font-weight: 500;
   font-display: swap;
