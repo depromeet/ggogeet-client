834c32af641051c8278ccfa5d34633e21fcf621e
ding-co <pangoons@naver.com>
2022-12-19 20:12:58 +0900

    💄 편지 작성 - ReceiverFriendsForm 기본 구조 UI 셋업

--- src/components/features/LetterWrite/InputRecipient/Forms/ReceiverFriendsForm.tsx
+++ src/components/features/LetterWrite/InputRecipient/Forms/ReceiverFriendsForm.tsx
@@ -1,7 +1,15 @@
 import { ReactElement } from "react";
+import { useBottomButton } from "../Hooks";
+import * as S from "../styled";
 
 const ReceiverFriendsForm = (): ReactElement => {
-  return <div></div>;
+  const bottomButton = useBottomButton({text: '친구목록에 없어요'});
+  return (
+    <>
+      <S.LetterWriteH1>누구에게 보낼 건가요?</S.LetterWriteH1>
+      {bottomButton}
+    </>
+  );
 };
 
 export default ReceiverFriendsForm;
