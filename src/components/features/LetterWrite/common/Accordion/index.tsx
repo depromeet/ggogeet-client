2ccf61c4e175825e0d03065c5b4dfb90b074241a
ding-co <pangoons@naver.com>
2022-12-25 15:00:30 +0900

    💄 LetterWrite - remind UI commpleted with Accordion component

--- src/components/features/LetterWrite/common/Accordion/index.tsx
+++ src/components/features/LetterWrite/common/Accordion/index.tsx
@@ -1,4 +1,6 @@
 import { RemindDataType } from "@/src/data/LetterWrite/type";
+import Image from "next/image";
+import { useState } from "react";
 import * as S from "../styled";
 
 interface AccordionProps {
@@ -6,10 +8,42 @@ interface AccordionProps {
 }
 
 const Accordion = ({ data }: AccordionProps) => {
+  const [clickedMemoId, setClickedMemoId] = useState<number>(-1);
+  const onClickMemoId = (id: number) => {
+    if (clickedMemoId === id) {
+      setClickedMemoId(-1);
+    } else {
+      setClickedMemoId(id);
+    }
+  };
   return (
     <S.LetterWriteAccordionWrapper>
       {data.map((memo) => (
-        <li key={memo.id}></li>
+        <li key={memo.id}>
+          <button onClick={() => onClickMemoId(memo.id)}>
+            <div>
+              {memo.isAlarm && (
+                <Image
+                  src="/Icons/icon__accordion-bell.svg"
+                  alt="아코디언 알림"
+                  width={16}
+                  height={16}
+                />
+              )}
+              <span>{memo.title}</span>
+            </div>
+            <div>
+              <span>{memo.created_at}</span>
+              <Image
+                src="/Icons/icon__accordion-chevron-down.svg"
+                alt="아코디언 화살표"
+                width={12}
+                height={12}
+              />
+            </div>
+          </button>
+          {clickedMemoId === memo.id && <p>{memo.description}</p>}
+        </li>
       ))}
     </S.LetterWriteAccordionWrapper>
   );
--- src/components/features/LetterWrite/common/styled.ts
+++ src/components/features/LetterWrite/common/styled.ts
@@ -1,3 +1,5 @@
+import { Body1, Body2, Body4 } from "@/src/styles/commons";
+import theme from "@/src/styles/theme";
 import styled from "@emotion/styled";
 
 const LetterWriteTagContainerWrapper = styled.div`
@@ -6,6 +8,56 @@ const LetterWriteTagContainerWrapper = styled.div`
   gap: 8px 12px;
 `;
 
-const LetterWriteAccordionWrapper = styled.ul``;
+const LetterWriteAccordionWrapper = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  li {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    background-color: ${theme.colors.navy30};
+    border-radius: ${theme.radius.md};
+    padding: 12px 16px;
+    gap: 8px;
+    button {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      & > div:first-of-type {
+        max-width: 70%;
+        display: flex;
+        align-items: center;
+        gap: 4px;
+        span {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          line-height: 150%;
+          letter-spacing: -0.5px;
+          ${Body4}
+          color: ${theme.colors.white};
+        }
+      }
+      & > div:last-of-type {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        span {
+          line-height: 14px;
+          letter-spacing: -0.2px;
+          ${Body1}
+          color: ${theme.colors.gray3};
+        }
+      }
+    }
+    p {
+      line-height: 150%;
+      letter-spacing: -0.2px;
+      color: ${theme.colors.gray2};
+      ${Body2};
+    }
+  }
+`;
 
 export { LetterWriteTagContainerWrapper, LetterWriteAccordionWrapper };
