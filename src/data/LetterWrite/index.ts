dbe77727fb90818e746be8a6ba43fb2bbbcd6160
ding-co <pangoons@naver.com>
2022-12-25 13:32:40 +0900

    💄 data - LetterWrite type and data modified

--- src/data/LetterWrite/index.ts
+++ src/data/LetterWrite/index.ts
@@ -1,69 +1,97 @@
-import type { guidelineBasicDataType, SituationTemplateDataType } from "./type";
+import type { SituationGuidelineSentenceType } from "@/src/types/Letter";
+import type { SituationTemplateDataType } from "./type";
 
+// TODO: description 수정 필요
 const situationTemplatesData: SituationTemplateDataType = [
   {
-    type: "축하",
-    value: "축하",
-    imageUrl: "",
+    situationId: 1,
+    templateUrl: "",
+    title: "축하하는 고양이",
+    description:
+      "응원하고 싶을 때는 물개 친구를 건네보세요. 신나는 물개 박수 어쩌 할거예요!",
   },
   {
-    type: "위로",
-    value: "위로",
-    imageUrl: "",
+    situationId: 2,
+    templateUrl: "",
+    title: "감동받은 곰",
+    description:
+      "응원하고 싶을 때는 물개 친구를 건네보세요. 신나는 물개 박수 어쩌 할거예요!",
   },
   {
-    type: "사랑해요",
-    value: "사랑해요",
-    imageUrl: "",
+    situationId: 3,
+    templateUrl: "",
+    title: "사랑이 넘치는 토끼",
+    description:
+      "응원하고 싶을 때는 물개 친구를 건네보세요. 신나는 물개 박수 어쩌 할거예요!",
   },
   {
-    type: "응원",
-    value: "응원",
-    imageUrl: "",
+    situationId: 4,
+    templateUrl: "",
+    title: "사과하는 다람쥐",
+    description:
+      "응원하고 싶을 때는 물개 친구를 건네보세요. 신나는 물개 박수 어쩌 할거예요!",
+  },
+  {
+    situationId: 5,
+    templateUrl: "",
+    title: "소식 전하는 비둘기",
+    description:
+      "응원하고 싶을 때는 물개 친구를 건네보세요. 신나는 물개 박수 어쩌 할거예요!",
+  },
+  {
+    situationId: 6,
+    templateUrl: "",
+    title: "응원하는 물개",
+    description:
+      "응원하고 싶을 때는 물개 친구를 건네보세요. 신나는 물개 박수 어쩌 할거예요!",
+  },
+  {
+    situationId: 7,
+    templateUrl: "",
+    title: "친해지고 싶은 펭귄",
+    description:
+      "응원하고 싶을 때는 물개 친구를 건네보세요. 신나는 물개 박수 어쩌 할거예요!",
+  },
+  {
+    situationId: 8,
+    templateUrl: "",
+    title: "꼬깃",
+    description:
+      "응원하고 싶을 때는 물개 친구를 건네보세요. 신나는 물개 박수 어쩌 할거예요!",
   },
 ];
 
-const guidelineBasicData: guidelineBasicDataType = [
-  {
-    type: "축하",
-    value: "기본 제공 문장 1",
+// 임시용 (감동받은 곰에 해당하는 가이드라인 데이터)
+const tempGuidelineData: SituationGuidelineSentenceType = {
+  userSentence: {
+    situation_id: 2,
+    sentence: [],
   },
-  {
-    type: "축하",
-    value: "기본 제공 문장 2",
+  guideSentence: {
+    situation_id: 2,
+    sentence: [
+      {
+        id: 1,
+        content: "기본 제공 문장 1",
+      },
+      {
+        id: 2,
+        content: "기본 제공 문장 2",
+      },
+      {
+        id: 3,
+        content: "기본 제공 문장 3",
+      },
+      {
+        id: 4,
+        content: "기본 제공 문장 4",
+      },
+      {
+        id: 5,
+        content: "기본 제공 문장 5",
+      },
+    ],
   },
-  {
-    type: "축하",
-    value: "기본 제공 문장 3",
-  },
-  {
-    type: "축하",
-    value: "기본 제공 문장 4",
-  },
-  {
-    type: "축하",
-    value: "기본 제공 문장 5",
-  },
-  {
-    type: "축하",
-    value: "기본 제공 문장 6",
-  },
-  {
-    type: "축하",
-    value: "기본 제공 문장 7",
-  },
-  {
-    type: "축하",
-    value: "기본 제공 문장 8",
-  },
-  {
-    type: "축하",
-    value: "기본 제공 문장 9",
-  },
-  {
-    type: "축하",
-    value: "기본 제공 문장 10",
-  },
-];
+};
 
-export { situationTemplatesData, guidelineBasicData };
+export { situationTemplatesData, tempGuidelineData };
--- src/data/LetterWrite/type.d.ts
+++ src/data/LetterWrite/type.d.ts
@@ -1,13 +1,18 @@
-export type TemplateType = "축하" | "위로" | "사랑해요" | "응원";
+type TemplateType =
+  | "축하하는 고양이"
+  | "감동받은 곰"
+  | "사랑이 넘치는 토끼"
+  | "사과하는 다람쥐"
+  | "소식 전하는 비둘기"
+  | "응원하는 물개"
+  | "친해지고 싶은 펭귄"
+  | "꼬깃";
 
-export type SituationTemplateDataType = Array<{
-  type: TemplateType;
-  value: TemplateType;
-  imageUrl: string;
+type SituationTemplateDataType = Array<{
+  situationId: number;
+  templateUrl: string;
+  title: TemplateType;
+  description: string;
 }>;
 
-export type guidelineBasicDataType = Array<{
-  type: TemplateType;
-  value: string;
-  isCustom?: boolean;
-}>;
+export { SituationTemplateDataType };
