cea01bd45eac57aa7bb6da073b0c0cb0db189488
KimGaeun <greencloud11@hanyang.ac.kr>
2022-12-26 16:55:06 +0900

    💄 받은편지추가 페이지 textArea, bottomButton 추가

--- src/pages/letter-storage/add.tsx
+++ src/pages/letter-storage/add.tsx
@@ -1,22 +1,56 @@
+import BottomButton from "@/src/components/common/Buttons/BottomButton";
+import Textarea from "@/src/components/common/Textarea";
 import TopNavigation from "@/src/components/common/TopNavigation";
 import { NavBack } from "@/src/components/common/TopNavigation/atoms";
 import AddNavigationBar from "@/src/components/features/letterStorage/add/AddNavigationBar";
-import { Display2 } from "@/src/styles/commons";
+import { Body4, Display2 } from "@/src/styles/commons";
 import styled from "@emotion/styled";
 
 const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
   background-color: ${({ theme }) => theme.colors.navy};
   height: 100vh;
-`;
-
-const MainLayout = styled.div`
-  display: flex;
-  justify-content: center;
 `;
 
 const TopNavigationTitle = styled.p`
   color: ${({ theme }) => theme.colors.white};
   ${Display2}
+`;
+
+const MainLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px 20px 0 20px;
+  flex: 1;
+`;
+
+const LetterAddTextAreaWrapper = styled.div`
+  margin: 20px 0 0 0;
+`;
+
+const LetterAddTextArea = styled(Textarea)`
+  height: 320px;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const BottomLayout = styled.div`
+  display: flex;
+  padding: 20px;
+`;
+
+interface NextButtonProps {
+  canClicked: boolean;
+}
+
+const NextButton = styled(BottomButton)<NextButtonProps>`
+  background-color: ${({ theme }) => theme.colors.gray5};
+  color: ${({ theme, canClicked }) =>
+    canClicked ? theme.colors.white : theme.colors.gray3};
+  border-radius: 2px;
+
+  ${Body4}
 `;
 
 const LetterStorageAddPage = () => {
@@ -26,9 +60,17 @@ const LetterStorageAddPage = () => {
         title={<TopNavigationTitle>받은 편지 추가</TopNavigationTitle>}
         leftElem={<NavBack color="white" />}
       />
+
       <MainLayout>
         <AddNavigationBar selectedItem="텍스트로 추가" />
+        <LetterAddTextAreaWrapper>
+          <LetterAddTextArea />
+        </LetterAddTextAreaWrapper>
       </MainLayout>
+
+      <BottomLayout>
+        <NextButton name="다음" canClicked={true} />
+      </BottomLayout>
     </Layout>
   );
 };
