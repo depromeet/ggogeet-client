e5c768a932787b3ec7d9e390106944083bfa17b5
KimGaeun <greencloud11@hanyang.ac.kr>
2022-12-26 23:38:46 +0900

    💄 꼬깃기억 페이지 UI 수정

--- src/pages/letter-remind/index.tsx
+++ src/pages/letter-remind/index.tsx
@@ -1,7 +1,9 @@
 import Button from "@/src/components/common/Buttons/Button";
 import TopNavigation from "@/src/components/common/TopNavigation";
-import TodoContainer from "@/src/components/features/letterRemind/TodoContainer";
+import { NavBack } from "@/src/components/common/TopNavigation/atoms";
+import TodoContainer from "@/src/components/features/letter-remind/TodoContainer";
 import { RemindData } from "@/src/data/LetterRemind";
+import { Display2 } from "@/src/styles/commons";
 import styled from "@emotion/styled";
 import Image from "next/image";
 
@@ -25,19 +27,17 @@ const TodoContainerWrapper = styled.div`
   padding: 0 0 12px 0;
 `;
 
+const TopNavigationTitle = styled.p`
+  color: ${({ theme }) => theme.colors.white};
+  ${Display2}
+`;
+
 const LetterRemindPage = () => {
   return (
     <Layout>
       <TopNavigation
-        title="꼬깃기억"
-        rightElem={
-          <Image
-            src="/icons/hamburger.svg"
-            alt="햄버거아이콘"
-            width={18}
-            height={16}
-          />
-        }
+        title={<TopNavigationTitle>꼬깃 기억</TopNavigationTitle>}
+        leftElem={<NavBack color="white" />}
       />
 
       <MainLayout>
