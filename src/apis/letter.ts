4d0e21a254c053363e6cbf31ab91aeee8abe0371
ding-co <pangoons@naver.com>
2023-01-08 16:07:03 +0900

    👽️ API - postSendLetterTempCompleted added

--- src/apis/letter.ts
+++ src/apis/letter.ts
@@ -1,9 +1,15 @@
 import { HTTP_METHOD } from "../constants/api";
-import { CreatedLetterDataType } from "../types/letter";
+import {
+  CreatedLetterType,
+  PostNewLetterCreateType,
+  PostSendLetterCompleteType,
+  PostSendLetterTempCompleteType,
+} from "../types/letter";
+
 import { requester } from "./requester";
 
-export const postNewLetterCreate = async (payload: CreatedLetterDataType) => {
-  const { data } = await requester<unknown>({
+export const postNewLetterCreate = async (payload: CreatedLetterType) => {
+  const { data } = await requester<PostNewLetterCreateType>({
     method: HTTP_METHOD.POST,
     url: `/letters/draft`,
     data: payload,
@@ -12,9 +18,18 @@ export const postNewLetterCreate = async (payload: CreatedLetterDataType) => {
 };
 
 export const postSendLetterComplete = async (letterId: number) => {
-  const { data } = await requester<number>({
+  const { data } = await requester<PostSendLetterCompleteType>({
     method: HTTP_METHOD.POST,
     url: `/letters/${letterId}/complete`,
   });
+  return data;
+};
+
+export const postSendLetterTempComplete = async (letterId: number) => {
+  const { data } = await requester<PostSendLetterTempCompleteType>({
+    method: HTTP_METHOD.POST,
+    url: `/letters/${letterId}/temp-complete`,
+  });
+
   return data;
 };
