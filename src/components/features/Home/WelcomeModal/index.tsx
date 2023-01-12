b4577ee05cc14235ee6f7ac12c5aa1c74fbcec5d
dongkyuuuu <desktop@kakao.com>
2023-01-12 23:45:16 +0900

    🐛 gif 용량 줄임

--- src/components/features/Home/WelcomeModal/index.tsx
+++ src/components/features/Home/WelcomeModal/index.tsx
@@ -13,32 +13,32 @@ const welcomeModalContents: {
 }[] = [
   {
     description: "반가워요!\n꼬깃에 오신 것을 환영해요.",
-    image: "/gifs/ggogeet_welcome1.gif",
+    image: "/gifs/ggogeet_welcome1-0.gif",
     width: 64,
     height: 64,
   },
   {
     description: "어떤 내용을 써야할지 망설여질 때\n꼬깃 가이드로 도와줄게요.",
-    image: "/gifs/ggogeet_welcome2.gif",
+    image: "/gifs/ggogeet_welcome2-0.gif",
     width: 240,
     height: 155,
   },
   {
     description:
       "전하려는 마음을 메모해뒀다가\n잊지 않고 꼬깃을 보낼 수 있어요.",
-    image: "/gifs/ggogeet_welcome3.gif",
+    image: "/gifs/ggogeet_welcome3-0.gif",
     width: 240,
     height: 155,
   },
   {
     description: "여기저기 흩어져 있던\n마음들을 차곡차곡 보관할 수 있어요.",
-    image: "/gifs/ggogeet_welcome4.gif",
+    image: "/gifs/ggogeet_welcome4-0.gif",
     width: 240,
     height: 151,
   },
   {
     description: "꼬깃과 함께 편지를 써서\n소중한 마음을 주고받아볼까요?",
-    image: "/gifs/ggogeet_welcome5.gif",
+    image: "/gifs/ggogeet_welcome5-0.gif",
     width: 186,
     height: 62,
   },
